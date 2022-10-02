0da5200f7c74d4d3870fe869bc94ee996de6fd95
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-02 17:23:42 +0700

    handle cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "https://example.com",
+    origin: "http://localhost:300",
     methods: ["GET", "POST"]
   }
 });
