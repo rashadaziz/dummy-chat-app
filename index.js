d1ab6fa25f1e510a387dda9c3c1f2171db7ebbab
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-29 11:20:50 +0700

    change server

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
+const app = require("express")();
+const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require("socket.io")(http, {
+  cors: {
+    origin: `http://localhost:${port}`,
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
+  },
+  allowEIO3: true,
 });
 
-io.on('connection', (socket) => {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
   const transport = socket.conn.transport.name; // in most cases, "polling"
 
   socket.conn.on("upgrade", () => {
     const upgradedTransport = socket.conn.transport.name; // in most cases, "websocket"
   });
 
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
